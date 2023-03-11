514bad1dc4c6fa21833c3b904dbcda4af6af8daf
Sandamini Senevirathna <kgcsandamini@gmail.com>
2023-03-11 09:27:33 +0530

    create entry widget to display input/output

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import tkinter as tk
+
+class Calculator:
+    def __init__(self, master):
+        self.master = master
+        master.title("Calculator")
+
+        # create entry widget to display input/output
+        self.display = tk.Entry(master, width=25, font=('Arial', 16))
+        self.display.grid(row=0, column=0, columnspan=4, pady=10)
+
 
